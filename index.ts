02f47e97269d3d9d3c746f3dff14860877d0acb7
“Joshua <punshon.jm@live.com.au>
2021-02-15 22:07:55 +1000

    Extract layout function

--- index.ts
+++ index.ts
@@ -7,9 +7,10 @@ import R from 'escape-string-regexp';
 import { JSDOM } from 'jsdom';
 import { promises as fs } from 'fs';
 import writeFileAtomic from 'write-file-atomic';
-
 import html from 'tagged-template-noop';
+
 import createIdentifier from './src/lib/createIdentifier';
+import layout from './src/lib/layout';
 
 export const WEB_PORT = process.env.WEB_PORT ?? 8000;
 export const EMAIL_PORT = process.env.EMAIL_PORT ?? 2525;
@@ -152,61 +153,7 @@ export const emailServer = new SMTPServer({
   },
 }).listen(EMAIL_PORT);
 
-function layout(content: string): string {
-  return html`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Kill the Newsletter!</title>
-        <meta name="author" content="Leandro Facchinetti" />
-        <meta
-          name="description"
-          content="Convert email newsletters into Atom feeds."
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="${BASE_URL}/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="${BASE_URL}/favicon-16x16.png"
-        />
-        <link rel="icon" type="image/x-icon" href="${BASE_URL}/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="${BASE_URL}/styles.css" />
-      </head>
-      <body>
-        <header>
-          <h1><a href="${BASE_URL}/">Kill the Newsletter!</a></h1>
-          <p>Convert email newsletters into Atom feeds</p>
-          <p>
-            <img
-              src="${BASE_URL}/logo.svg"
-              alt="Convert email newsletters into Atom feeds"
-            />
-          </p>
-        </header>
-        <main>${content}</main>
-        <footer>
-          <p>
-            By <a href="https://leafac.com">Leandro Facchinetti</a> ·
-            <a href="https://github.com/leafac/kill-the-newsletter.com"
-              >Source</a
-            >
-            · <a href="${ISSUE_REPORT}">Report an Issue</a>
-          </p>
-        </footer>
-        <script src="${BASE_URL}/clipboard.min.js"></script>
-        <script src="${BASE_URL}/scripts.js"></script>
-      </body>
-    </html>
-  `.trim();
-}
+
 
 function newInbox(): string {
   return html`
---
+++ src/lib/layout.ts
@@ -0,0 +1,60 @@
+const BASE_URL = process.env.BASE_URL || 'http://localhost:8000';
+const ISSUE_REPORT = process.env.ISSUE_REPORT || 'mailto:kill-the-newsletter@leafac.com';
+
+const layout = (content: string): string => {
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Kill the Newsletter!</title>
+      <meta name="author" content="Leandro Facchinetti" />
+      <meta
+        name="description"
+        content="Convert email newsletters into Atom feeds."
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="${BASE_URL}/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="${BASE_URL}/favicon-16x16.png"
+      />
+      <link rel="icon" type="image/x-icon" href="${BASE_URL}/favicon.ico" />
+      <link rel="stylesheet" type="text/css" href="${BASE_URL}/styles.css" />
+    </head>
+    <body>
+      <header>
+        <h1><a href="${BASE_URL}/">Kill the Newsletter!</a></h1>
+        <p>Convert email newsletters into Atom feeds</p>
+        <p>
+          <img
+            src="${BASE_URL}/logo.svg"
+            alt="Convert email newsletters into Atom feeds"
+          />
+        </p>
+      </header>
+      <main>${content}</main>
+      <footer>
+        <p>
+          By <a href="https://leafac.com">Leandro Facchinetti</a> ·
+          <a href="https://github.com/leafac/kill-the-newsletter.com"
+            >Source</a
+          >
+          · <a href="${ISSUE_REPORT}">Report an Issue</a>
+        </p>
+      </footer>
+      <script src="${BASE_URL}/clipboard.min.js"></script>
+      <script src="${BASE_URL}/scripts.js"></script>
+    </body>
+  </html>`;
+
+  return html.trim();
+};
+
+export default layout;
