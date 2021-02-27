0742e05e77f715902d93d61f8e499ec51d06a804
“Joshua <punshon.jm@live.com.au>
2021-02-27 11:21:11 +1000

    Add datastore testing to start file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
+import { v4 } from 'uuid';
 import config from './config';
+
+import Feed from './datastore/Feed';
+
+import createNewFeed from './lib/createNewFeed';
 import app from './webServer/app';
 
-app.listen(config.webPort,
-  () => console.log(`Server started: ${config.baseUrl}`));
+const runWebServer = async () => {
+  app.listen(config.webPort, () => {
+    console.log(`Server started: ${config.baseUrl}`);
+  });
+
+  try {
+    const { entities: feeds } = await Feed.list();
+    console.log('feeds', feeds);
+
+    if (feeds.length === 0) {
+      const f = await createNewFeed({
+        title: `Test:${v4()}`,
+      });
+
+      console.log('f', JSON.stringify(f, null, 2));
+    }
+  } catch (error) {
+    console.log('error');
+    console.error(error);
+  }
+};
+
+runWebServer();
